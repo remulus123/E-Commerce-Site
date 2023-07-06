f081bee09b516a1311c59783787496ce2d92a325
remulus123 <murungiremulus73@gmail.com>
2023-07-06 10:02:23 +0300

    Add files via upload

---
+++ ecomsite/shop/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from.models import Products,Order
+
+
+# Register your models here.
+admin.site.site_header = "E-commerce Site" #customzing admin site
+admin.site.site_title = " ABC Shopping"
+admin.site.index_title = "Manage ABC Shopping"
+
+class ProductAdmin(admin.ModelAdmin):
+    def change_category_to_default(self,request,queryset):
+        queryset.update(category="default")
+    change_category_to_default.short_description = "Default Category"
+    list_display = ('title','price','discount_price','category','description')
+    search_fields = ('title',)
+    actions = ('change_category_to_default',)
+    fields = ('title','price',)
+    list_editable = ('price','category',)
+admin.site.register(Products,ProductAdmin)
+admin.site.register(Order)
